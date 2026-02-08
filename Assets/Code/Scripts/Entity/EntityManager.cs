806ba393088e60f025f5109f716f0a13b31fb282
loxenary <zanxub@gmail.com>
2026-02-08 11:25:05 +0700

    feat: balancing

--- Assets/Code/Scripts/Entity/EntityManager.cs
+++ Assets/Code/Scripts/Entity/EntityManager.cs
@@ -4,73 +4,89 @@ using UnityEngine;
 public class EntityManager : MonoBehaviour
 {
     [SerializeField] List<SheepCore> _entities = new List<SheepCore>();
-    [SerializeField] [ReadOnly] List<Herd> _herds = new List<Herd>();
-    [SerializeField] [ReadOnly] bool _isActive;
+    [SerializeField][ReadOnly] List<Herd> _herds = new List<Herd>();
+    [SerializeField][ReadOnly] bool _isActive;
 
     public List<SheepCore> Entities => _entities;
 
-    void OnEnable() {
+    void OnEnable()
+    {
         GameManager.S_OnGameStateChanged += HandleGameStateChanged;
         HerdSpawner.S_OnHerdSpawned += HandleHerdSpawned;
         NeutralSpawner.S_OnNeutralSpawned += HandleNeutralSpawned;
     }
 
-    void OnDisable() {
+    void OnDisable()
+    {
         GameManager.S_OnGameStateChanged -= HandleGameStateChanged;
         HerdSpawner.S_OnHerdSpawned -= HandleHerdSpawned;
         NeutralSpawner.S_OnNeutralSpawned -= HandleNeutralSpawned;
     }
 
-    public void HandleGameStateChanged(GameState state) {
+    public void HandleGameStateChanged(GameState state)
+    {
         _isActive = state == GameState.Playing;
     }
 
-    void HandleHerdSpawned(Herd herd) {
+    void HandleHerdSpawned(Herd herd)
+    {
         _herds.Add(herd);
         herd.OnHerdEmpty += HandleHerdEmpty;
-        foreach(var member in herd.Members) {
+        foreach (var member in herd.Members)
+        {
             RegisterEntity(member);
         }
     }
 
-    void HandleNeutralSpawned(SheepCore entity) {
+    void HandleNeutralSpawned(SheepCore entity)
+    {
         RegisterEntity(entity);
     }
 
-    void RegisterEntity(SheepCore entity) {
+    void RegisterEntity(SheepCore entity)
+    {
         _entities.Add(entity);
         entity.OnDeath += HandleEntityDeath;
     }
 
-    void Update() {
-        if(!_isActive) return;
-        foreach(var herd in _herds) {
+    void Update()
+    {
+        if (!_isActive) return;
+        foreach (var herd in _herds)
+        {
             herd.DoUpdate();
         }
-        foreach(var entity in _entities) {
+        foreach (var entity in _entities)
+        {
             entity.DoUpdate();
         }
     }
 
-    void FixedUpdate() {
-        if(!_isActive) return;
-        foreach(var entity in _entities) {
+    void FixedUpdate()
+    {
+        if (!_isActive) return;
+        foreach (var entity in _entities)
+        {
             entity.DoFixedUpdate();
         }
     }
 
-    [SerializeField] float _trustDecreaseOnDeath = 1f;
-    void HandleEntityDeath(SheepCore entity) {
+    [SerializeField] float _trustDecreaseOnDeath = 0.25f;
+    void HandleEntityDeath(SheepCore entity)
+    {
         _entities.Remove(entity);
         // Decrease trust of all other friendly sheep
-        foreach(var otherEntity in _entities) {
-            if(otherEntity.Stats.TeamID == entity.Stats.TeamID) {
+        foreach (var otherEntity in _entities)
+        {
+            if (otherEntity.Stats.TeamID == entity.Stats.TeamID)
+            {
                 otherEntity.DecreaseTrust(_trustDecreaseOnDeath);
             }
         }
     }
 
-    void HandleHerdEmpty(Herd herd) {
+    void HandleHerdEmpty(Herd herd)
+    {
         _herds.Remove(herd);
     }
 }
--- Assets/Code/Scripts/Presets/StateMachine/Sheep/SheepCore.cs
+++ Assets/Code/Scripts/Presets/StateMachine/Sheep/SheepCore.cs
@@ -13,11 +13,13 @@ public class SheepCore : Core<SheepCore, SheepStates>, ISignInteractable
     Herd _herd;
     public Herd Herd => _herd;
 
-    public void SetHerd(Herd herd) {
+    public void SetHerd(Herd herd)
+    {
         _herd = herd;
     }
 
-    public void Init(HerdData data) {
+    public void Init(HerdData data)
+    {
         _moveSpeed = data.MoveSpeed;
     }
 
@@ -33,9 +35,11 @@ public class SheepCore : Core<SheepCore, SheepStates>, ISignInteractable
     {
         Attack.PlayHurtAnimation();
         Stats.CurrentHealth -= hitRequest.Damage;
-        if(Stats.CurrentHealth <= 0) {
+        if (Stats.CurrentHealth <= 0)
+        {
             Die();
-            if(hitRequest.Element == Element.Bomb) {
+            if (hitRequest.Element == Element.Bomb)
+            {
                 ApplyTrustToWitness();
             }
             s_OnSheepDeath();
@@ -56,50 +60,58 @@ public class SheepCore : Core<SheepCore, SheepStates>, ISignInteractable
     public float MoveSpeedMultiplier { get => _moveSpeedMultiplier; set => _moveSpeedMultiplier = value; }
     public float MoveSpeed => _moveSpeed * _moveSpeedMultiplier;
 
-    public void DoFixedUpdate() {
-        if(CurrentState == null) return;
+    public void DoFixedUpdate()
+    {
+        if (CurrentState == null) return;
         CurrentState.StateFixedUpdate();
     }
-    public void DoUpdate() {
-        if(CurrentState == null) return;
+    public void DoUpdate()
+    {
+        if (CurrentState == null) return;
         CurrentState.StateUpdate();
 
         Stats.CurrentHunger -= Stats.HungerDecreasePerSecond * Time.deltaTime;
     }
 
-    [SerializeField] [ReadOnly] List<Sign> _currentSigns = new ();
+    [SerializeField][ReadOnly] List<Sign> _currentSigns = new();
     public List<Sign> CurrentSigns => _currentSigns;
-    public Sign GetLatestSign() {
-        if(_currentSigns.Count == 0) return null;
-        return _currentSigns[_currentSigns.Count-1];
+    public Sign GetLatestSign()
+    {
+        if (_currentSigns.Count == 0) return null;
+        return _currentSigns[_currentSigns.Count - 1];
     }
 
 
 
     Sign _lastSignThatCausedAlert = null;
     public Sign LastSignThatCausedAlert { get => _lastSignThatCausedAlert; set => _lastSignThatCausedAlert = value; }
-    public void OnSignEnter(Sign sign) {
+    public void OnSignEnter(Sign sign)
+    {
         _currentSigns.Add(sign);
-        if(_currentSigns.Count >= 1) {
+        if (_currentSigns.Count >= 1)
+        {
             // if alert twice with same sign, ignore
-            if(_lastSignThatCausedAlert == sign) return;
+            if (_lastSignThatCausedAlert == sign) return;
             _lastSignThatCausedAlert = sign;
             SwitchState(States.Alert);
         }
     }
 
-    public void OnSignExit(Sign sign) {
+    public void OnSignExit(Sign sign)
+    {
         _currentSigns.Remove(sign);
-        if(_currentSigns.Count == 0) {
+        if (_currentSigns.Count == 0)
+        {
             _rb.linearVelocity = Vector2.zero;
-            if(CurrentState != States.Death) SwitchState(States.Wander);
+            if (CurrentState != States.Death) SwitchState(States.Wander);
         }
     }
 
     public Action<SheepCore> OnDeath;
     bool _isDead = false;
-    public void Die() {
-        if(_isDead) return;
+    public void Die()
+    {
+        if (_isDead) return;
         _isDead = true;
         SetActiveCore(false);
         SwitchState(States.Death);
@@ -111,58 +123,73 @@ public class SheepCore : Core<SheepCore, SheepStates>, ISignInteractable
 
     [Header("Witness Trust Settings")]
     [SerializeField] float _trustOnWitnessDeath = 10f; // TODO: edit this
-    [SerializeField] float _witnessRadius = 5f;
-    void ApplyTrustToWitness() {
+    [SerializeField] float _witnessRadius = 8f;
+    void ApplyTrustToWitness()
+    {
         Collider2D[] hits = Physics2D.OverlapCircleAll(transform.position, _witnessRadius);
-        foreach(var hit in hits) {
-            if(hit.TryGetComponent<SheepCore>(out var playerCore)) {
+        foreach (var hit in hits)
+        {
+            if (hit.TryGetComponent<SheepCore>(out var playerCore))
+            {
                 playerCore.IncreaseTrust(_trustOnWitnessDeath);
             }
         }
     }
-    public void IncreaseTrust(float amount) {
+    public void IncreaseTrust(float amount)
+    {
         Stats.CurrentTrust += amount;
         // TODO: play some VFX or SFX
     }
-    public void DecreaseTrust(float amount) { // called when friendly sheep die
+    public void DecreaseTrust(float amount)
+    { // called when friendly sheep die
         Stats.CurrentTrust -= amount;
     }
 
     [SerializeField] EntityConverter _converter;
-    public void ConvertToEnemy() {
+    public void ConvertToEnemy()
+    {
         Stats.State = EntityType.Enemy;
         _converter.ConvertToEnemy();
     }
-    public void ConvertToFriendly() {
+    public void ConvertToFriendly()
+    {
         Stats.State = EntityType.Friendly;
         _converter.ConvertToFriendly();
     }
-    public void ConvertToNeutral() {
+    public void ConvertToNeutral()
+    {
         Stats.State = EntityType.Neutral;
         _converter.ConvertToNeutral();
     }
 
     [SerializeField] Collider2D[] _col;
     [SerializeField] GameObject[] _objs;
-    public void SetActiveCore(bool isActive) {
-        if(isActive) {
+    public void SetActiveCore(bool isActive)
+    {
+        if (isActive)
+        {
             _rb.bodyType = RigidbodyType2D.Dynamic;
-            foreach(var col in _col) {
+            foreach (var col in _col)
+            {
                 col.enabled = true;
             }
-            foreach(var obj in _objs) {
+            foreach (var obj in _objs)
+            {
                 obj.SetActive(true);
             }
             States = new SheepStates(this);
             CurrentState = States.Wander;
             CurrentState.StateEnter();
         }
-        else {
+        else
+        {
             _rb.bodyType = RigidbodyType2D.Kinematic;
-            foreach(var col in _col) {
+            foreach (var col in _col)
+            {
                 col.enabled = false;
             }
-            foreach(var obj in _objs) {
+            foreach (var obj in _objs)
+            {
                 obj.SetActive(false);
             }
         }
@@ -170,7 +197,8 @@ public class SheepCore : Core<SheepCore, SheepStates>, ISignInteractable
 
     [SerializeField] AnimationUI _fallFromSkyAUI;
     [ContextMenu("Play Fall From Sky Animation")]
-    public void PlayFallFromSkyAnimation() {
+    public void PlayFallFromSkyAnimation()
+    {
         _fallFromSkyAUI.Play();
         ApplyTrustToWitness();
     }
