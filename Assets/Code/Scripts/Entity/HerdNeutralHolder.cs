94cd34e55bc4fc776e42c3f2c7d6dbed77e3caff
DhafinFawwaz <dhafin.fawwaz@gmail.com>
2026-02-08 10:29:51 +0700

    feat: vfxs

--- Assets/Code/Scripts/Entity/HerdNeutralHolder.cs
+++ Assets/Code/Scripts/Entity/HerdNeutralHolder.cs
@@ -5,8 +5,10 @@ using UnityEngine.Tilemaps;
 public class HerdNeutralHolder : MonoBehaviour
 {
     public static Action<SheepCore> S_OnNeutralConverted;
+    public static Action S_OnConverted;
 
     [SerializeField] SheepCore _owner;
+    [SerializeField] ParticleSystem _convertVFX;
 
     void OnTriggerEnter2D(Collider2D other)
     {
@@ -27,6 +29,9 @@ public class HerdNeutralHolder : MonoBehaviour
             sheep.ConvertToFriendly();
         else
             sheep.ConvertToEnemy();
+        
+        _convertVFX.Play();
+        S_OnConverted?.Invoke();
 
         sheep.Stats.TeamID = herd.TeamID;
         sheep.transform.SetParent(herd.transform);
